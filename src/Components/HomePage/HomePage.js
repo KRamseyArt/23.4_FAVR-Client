056f48347bb4046a82be749d1e48cdf8f58cd8ba
Kalin Ramsey <kalinramsey@gmail.com>
2020-08-02 09:55:20 -0700

    Added Favicon

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -14,8 +14,8 @@ export class HomePage extends Component {
         <Hero />
         <ul id="HomeInfo">
           <li>
-            <h3>Heading 1</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacinia, nisi vel aliquet cursus, nunc augue sodales est, sit amet consequat elit libero et tellus. Donec mollis ipsum sed sollicitudin aliquet. Maecenas sem purus, hendrerit ac pharetra in, elementum eget ex. Cras tincidunt risus tincidunt, consectetur turpis eget, dictum orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed aliquet dui magna, iaculis gravida ipsum aliquam nec. Fusce finibus cursus ante, ac mollis mi lobortis et.</p>
+            <h3>Welcome to FAVR!</h3>
+            <p>FAVR was developed as a way to help users keep track of the various tasks they dole out to others, and that others ask of themselves. By having a visual representation of the frequency with which others depend upon you, users are provided a tool to help gague and manage their social interactions.</p>
           </li>
           <li>
             <h3>Heading 2</h3>
